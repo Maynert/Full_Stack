410369ed65be8ee5acb884fbac4c438f336163fd
Maynert <67560248+Maynert@users.noreply.github.com>
2026-01-31 17:47:24 -0300

    Delete 10_PragramacaoWeb directory

--- 10_PragramacaoWeb/pwa/pwa.js
+++
@@ -1,9 +0,0 @@
-async function registrarServiceWorker() {
-  if (navigator.serviceWorker) {
-    try {
-      await navigator.serviceWorker.register("pwa-serviceWorker.js");
-    } catch (e) {
-      console.error(`Erro ao registrar o service worker: ${e}`);
-    }
-  }
-}
